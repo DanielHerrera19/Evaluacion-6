7744726a618a4d0f62a3568a7a944f9c6eff1d54
Daniel <Daniel@DESKTOP-KKC0TQU>
2023-09-08 15:24:35 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el monto total de su compra: ");
+            string montotexto = Console.ReadLine();
+
+            float monto= float.Parse(montotexto);
+            float descuento = 0f;
+
+            if (monto > 1000) 
+            {
+                descuento = monto * 0.20f;
+            }
+            else if (monto > 500)
+            {
+                descuento = monto * 0.10f;
+            }
+            else if (monto <= 500)
+            {
+                descuento = 0;
+            }
+
+            Console.WriteLine("Su descuento es de: " + descuento);
+            Console.ReadKey();
+        }
+    }
+}
